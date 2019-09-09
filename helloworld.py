31f876117ba883b13aa5ea31f2f3f71677c36fc8
zbass <zbass@umich.edu>
2019-09-09 17:49:25 -0400

    Updated to python 3

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 import random
-print "Zeke Bass"
+print ("Zeke Bass")
 x = random.randint(1, 100)
 y = random.randint(1, 100)
-print x
-print y
-print "Sum =", x + y
-print "Average =", (x + y) / 2.0
+print (x)
+print (y)
+print ("Sum =", x + y)
+print ("Average =", (x + y) / 2.0)
